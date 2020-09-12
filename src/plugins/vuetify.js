b8c3fc7b10112575529f5a535a3691e95b6551cb
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-09-12 23:20:03 +0900

    Add: vuetify primary and accent color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import { colors } from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.lime.darken4,
+        // TODO: set secondary #524900
+        accent: colors.white,
+      }
+    }
+  }
 });
