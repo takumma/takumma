26549f328a09377bd96439ba6c9dbf78907c4d67
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-09-13 11:39:50 +0900

    Add: App bar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" class="white--text">
-      <v-app-bar-nav-icon color="white"/>
+      
       <v-toolbar-title>takumma</v-toolbar-title>
 
       <v-divider/>
-
-      <v-toolbar-items>
-        <v-btn color="secondary" text>
-          GITHUB
-          <v-icon color="white">mdi-github-face</v-icon>
-        </v-btn>
-        <v-btn text>Support<v-icon>mdi-menu-down</v-icon></v-btn>
-      </v-toolbar-items>
+      
+      <v-app-bar-nav-icon color="white"/>
+      
     </v-app-bar>
     <v-main>
+      <router-view/>
     </v-main>
   </v-app>
 </template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-list>
+        <v-btn text>GITHUB<v-icon>mdi-github</v-icon></v-btn>
+    </v-list>
   </div>
 </template>
