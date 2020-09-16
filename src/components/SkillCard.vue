da04ed256e4e3bc422391e54eea9c53cc628ed4a
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-09-16 16:31:23 +0900

    Add: v-hover of skillcard

--- src/components/SkillCard.vue
+++ src/components/SkillCard.vue
@@ -1,14 +1,19 @@
 <template>
+<v-hover
+  v-slot:default="{ hover }"
+>
 	<v-card
 		tile
 		height="128"
 		width="128"
+		:elevation="hover ? 8 : 2"
 	>
 		<v-card-title class="justify-center" color="softBlack">
 			{{ skill }}
 			<v-icon size="64" color="softBlack">{{ icon }}</v-icon>	
-		</v-card-title>	
+		</v-card-title>
 	</v-card>
+</v-hover>
 </template>
 
 <script lang='ts'>
