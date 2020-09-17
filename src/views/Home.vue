22148af65de7fa0d228e418250ac35983701130b
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-09-17 11:36:38 +0900

    Add: skills list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
+    <v-row>
+      <v-col :cols="skills.length" v-for="skill in skills" :key="skill.name">
+        <skill-card :skill="skill.name" :icon="skill.icon" :color="skill.color" />
+      </v-col>
+    </v-row>
     <skill-card skill="Vue.js" icon="mdi-vuejs" color="green"/>
   </div>
 </template>
@@ -7,11 +12,17 @@
 <script lang='ts'>
 import Vue from 'vue';
 import SkillCard from '../components/SkillCard.vue';
+import skillData from '../assets/data/skill.json';
 
 export default Vue.extend({
   name: 'Home',
   components: {
     SkillCard,
+  },
+  data() {
+    return {
+      skills: skillData.skills
+    }
   }
 })
 </script>
