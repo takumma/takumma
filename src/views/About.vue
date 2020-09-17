08d3f170b5e29e067638b7b15009a0b6b5080de0
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-09-17 23:59:03 +0900

    Remove: some unnecessary codes

--- src/views/About.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <v-list>
-        <v-btn text>GITHUB<v-icon>mdi-github</v-icon></v-btn>
-    </v-list>
-  </div>
-</template>
