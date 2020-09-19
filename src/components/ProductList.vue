5f751f5966854d46836082d9cee974a8fadad593
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-09-19 22:35:03 +0900

    Update: change ProductList v-row → v-list

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="product-list">
-    <v-container fluid>
-      <v-row
-        align="center"
-        justify="center"
-      >
-        <v-col
-          :cols="products.length"
-          v-for="product in products"
-          :key="product.name"
-        >
-          <product-list-card
-            :name="product.name"
-            :memo="product.memo"
-          />
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
+	<div class="product-list">
+		<v-list
+			align="center"
+			justify="center"
+		>
+			<v-list-item
+				:cols="products.length"
+				v-for="product in products"
+				:key="product.name"
+			>
+				<product-list-card
+					:name="product.name"
+					:memo="product.memo"
+				/>
+			</v-list-item>
+		</v-list>
+	</div>
 </template>
 
 <script lang='ts'>
@@ -26,15 +24,15 @@ import ProductListCard from '../components/ProductListCard.vue';
 import ProductData from '../assets/data/product.json';
 
 export default Vue.extend({
-  name: 'Home',
-  components: {
-    ProductListCard,
-  },
-  data() {
-    return {
-      products: ProductData.products,
-      others: ProductData.others,
-    }
-  }
+	name: 'Home',
+	components: {
+		ProductListCard,
+	},
+	data() {
+		return {
+			products: ProductData.products,
+			others: ProductData.others,
+		}
+	}
 })
 </script>
