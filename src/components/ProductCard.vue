bdaf0ed6b2db47997ef1222b3f33c9cf43e0947a
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-09-19 17:10:05 +0900

    Add: create ProductCard component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-card
+		tile
+		height="150"
+		width="150"
+		elevation="2"
+	>
+	</v-card>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+
+export default Vue.extend({
+	props: {
+		name: String,
+		tech: String,
+		github: String,
+		url: String,
+		memo: String
+	}
+});
+</script>
