d9a5e31355f2c438376e0789ef7e3760f1288e19
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-09-21 22:15:35 +0900

    Add: btn of ProductListCard

--- src/components/ProductListCard.vue
+++ src/components/ProductListCard.vue
@@ -11,6 +11,19 @@
 				<v-list-item-subtitle class="text-body-1 mr-3 mb-3 ml-6 font-weight-black"> {{ memo }} </v-list-item-subtitle>
 			</v-list-item-content>
 		</v-list-item>
+
+		<v-card-actions>
+			<v-btn-toggle class="text-right mx-2">
+				<v-btn text>
+					<v-icon class="mr-1">mdi-github</v-icon>
+					github
+				</v-btn>
+				<v-btn text>
+					<v-icon class="mr-1">mdi-link</v-icon>
+					SITE URL
+				</v-btn>
+			</v-btn-toggle>
+		</v-card-actions>
 	</v-card>
 </template>
 
