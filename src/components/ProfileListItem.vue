642ac58568bf9f165a89170ef04723520010c6e0
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-09-22 14:31:55 +0900

    Add: create ProfileListItem comp

---
+++ src/components/ProfileListItem.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-list-item-content>
+      <v-list-item-title class="text-h5 chocorate--text">{{ title }}: {{ value }}</v-list-item-title>
+    </v-list-item-content>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    title: String,
+    value: String,
+  }
+})
+</script>
