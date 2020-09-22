bdc784883b973e086ebeadcccd0a8216fce6f539
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-09-22 14:43:43 +0900

    Add: profile listItem[]

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <v-list>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h5 chocorate--text">nickname: {{ profiles.nickname }}</v-list-item-title>
-        </v-list-item-content>
+      <v-list-item
+        v-for="item in listItem"
+        :key="item.name"
+      >
+        <profile-list-item />
       </v-list-item>
     </v-list>
   </div>
@@ -12,9 +13,22 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import ProfileListItem from '../components/ProfileListItem.vue';
 export default Vue.extend({
+  components: {
+    ProfileListItem,
+  },
   props: {
     profiles: Object,
+  },
+  data() {
+    listItem: [
+      { name: "NICKNAME", value: this.profiles.nickname },
+      { name: "REAL-NAME", value: this.profiles.real-name },
+      { name: "BIRTHDAY", value: this.profiles.birthday },
+      { name: "BELONG", value: this.profiles.belong },
+      { name: "INTEREST", value: this.profiles.interest },
+    ]
   }
 })
 </script>
