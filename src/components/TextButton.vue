be7dbf299e10a5ed88a68c8859ba6728f43acce7
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-09-22 11:24:59 +0900

    Add: create TextBtn component

---
+++ src/components/TextButton.vue
@@ -0,0 +1,26 @@
+<template>
+	<v-btn
+			text
+			color="chocorate"
+			:href="link"
+		>
+			<v-icon
+				class="mr-1"
+				color="chocorate"
+			>
+				{{ icon }}
+			</v-icon>
+			{{ text }}
+		</v-btn>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+	props: {
+		icon: String,
+		text: String,
+		link: String,
+	}
+})
+</script>
