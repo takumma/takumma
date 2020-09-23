feebbde4a4cad4371cc3ce447965d5bb401e699f
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-09-23 11:15:12 +0900

    Update: chip style when screen size is xs

--- src/components/TechChip.vue
+++ src/components/TechChip.vue
@@ -3,8 +3,8 @@
     class="ml-2 my-auto chocorate chocorate--text"
     outlined
   >
-    <v-icon class="mr-1">{{ tech.icon }}</v-icon>
-    {{ tech.name}}
+    <v-icon :class="$vuetify.breakpoint.xs ? '' : 'mr-1'">{{ tech.icon }}</v-icon>
+    {{ $vuetify.breakpoint.xs ? '' : tech.name }}
   </v-chip>
 </template>
 
@@ -13,6 +13,6 @@ import Vue from 'vue'
 export default Vue.extend({
   props: {
     tech: Object,
-  }
+  },
 })
 </script>
