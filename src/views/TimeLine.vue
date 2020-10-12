d52d7d721292b49e013169ffa7121390d3887b65
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-10-13 00:34:07 +0900

    Fix: TL

--- src/views/TimeLine.vue
+++ src/views/TimeLine.vue
@@ -1,13 +1,22 @@
 <template>
 	<div class="timeline">
+		<timeline class="text-left">
+    <timeline-title>title</timeline-title>
+    <timeline-item bg-color="#9dd8e0">item1</timeline-item>
+    <timeline-item :hollow="true">item2</timeline-item>
+  </timeline>
 	</div>
 </template>
 
 <script lang='ts'>
 import Vue from 'vue';
+import { Timeline, TimelineItem, TimelineTitle } from 'vue-cute-timeline';
 
 export default Vue.extend({
 	components: {
+		Timeline,
+    TimelineItem,
+    TimelineTitle
 	},
 })
 </script>
