aed517ff4fd1be33615784830e4b59239584fd18
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-10-12 21:39:43 +0900

    Add: create timeline.vue

---
+++ src/components/TimeLine.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="timeline">
+	</div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+
+export default Vue.extend({
+	components: {
+	},
+	props: {
+			activities: Array,
+	},
+})
+</script>
