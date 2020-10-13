43e8315643ac2e1385ce937b52ad3f5f072c1f67
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-10-13 22:34:53 +0900

    Remove: about timeline modules...

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,6 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import vuetify from './plugins/vuetify';
-import LightTimeline from 'vue-light-timeline';
-
-Vue.use(LightTimeline);
 
 Vue.config.productionTip = false
 
