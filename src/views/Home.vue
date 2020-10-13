d7adec878ddc8497830515729a1c9b98a2491d1d
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-10-13 23:40:23 +0900

    Add: timeline comp with v-timeline

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,23 @@
 <template>
   <div class="home text-center mt-12">
     
-    <title-text title="PROFILEs"/>
+    <title-text title="PROFILES"/>
     <profile
       class="mb-8"
       :profiles="data.profiles"
     />
-    <title-text title="SKILLs"/>
+    <title-text title="SKILLS"/>
     <skill-list
       class="mb-8"
       :skills="data.skills"
     />
-    <title-text title="PRODUCTs"/>
+    <title-text title="PRODUCTS"/>
     <product-list
       :products="data.products.products"
       :others="data.products.others"
     />
-
+    <title-text title="ACTIVITIES"/>
+    <timeline/>
   </div>
 </template>
 
@@ -26,6 +27,7 @@ import TitleText from '../components/TitleText.vue';
 import Profile from '../components/Profile.vue';
 import SkillList from '../components/SkillList.vue';
 import ProductList from '../components/ProductList.vue';
+import Timeline from '../components/Timeline.vue';
 import data from '../assets/data/data.json';
 
 export default Vue.extend({
@@ -34,7 +36,8 @@ export default Vue.extend({
     TitleText,
     Profile,
     SkillList,
-    ProductList
+    ProductList,
+    Timeline,
   },
   data() {
     return {
