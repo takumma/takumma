810fb198489972c491342759f118b73354c93a2e
takumma <takumma@users.noreply.github.com>
2020-11-21 14:42:43 +0000

    deploy: 9bae69ab1e11bef86fa76c9c3abaa5582037a5c3

---
+++ precache-manifest.77903445fa1b3866b135b36f072d9cf2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6139ea6ba35d07e5e70d",
+    "url": "css/app.6e68966a.css"
+  },
+  {
+    "revision": "e4ba44bba3c39d6dd283",
+    "url": "css/chunk-vendors.25aaf254.css"
+  },
+  {
+    "revision": "2a61b00fcc9b1afbf99be145777b1b72",
+    "url": "fonts/MaterialIcons-Regular.2a61b00f.eot"
+  },
+  {
+    "revision": "51cf1d641ae503ece727e7d84b182524",
+    "url": "fonts/MaterialIcons-Regular.51cf1d64.ttf"
+  },
+  {
+    "revision": "703cf8f274fbb265d49c6262825780e1",
+    "url": "fonts/MaterialIcons-Regular.703cf8f2.woff2"
+  },
+  {
+    "revision": "84a37de85c17f186652a179b1145392f",
+    "url": "fonts/MaterialIcons-Regular.84a37de8.woff"
+  },
+  {
+    "revision": "b0f2d9f20522f04fdd960f44d88d5ad8",
+    "url": "index.html"
+  },
+  {
+    "revision": "6139ea6ba35d07e5e70d",
+    "url": "js/app.5725f1c7.js"
+  },
+  {
+    "revision": "e4ba44bba3c39d6dd283",
+    "url": "js/chunk-vendors.d99a488a.js"
+  },
+  {
+    "revision": "f9e105bae10fd0359fbeb718a18d6ef7",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
