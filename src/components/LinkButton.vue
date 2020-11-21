89d3c542051b5d0d3e3caea3f275a25bcb4c8dc3
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-11-21 12:32:56 +0900

    Add: Linkbtn

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn>link</v-btn>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+interface Link {
+	icon: string;
+  color: string;
+  url: string;
+}
+
+@Component
+export default class LinkButton extends Vue{
+  @Prop()
+  readonly link!: Link;
+}
+</script>
