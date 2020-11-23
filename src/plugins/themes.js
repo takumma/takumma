606f166a23c25a258cd1dbc8c79429d65a661e76
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-11-24 00:12:06 +0900

    Add: themes.js

---
+++ src/plugins/themes.js
@@ -0,0 +1,20 @@
+import { colors } from 'vuetify/lib';
+
+export const themes = {
+    default: {
+        hojichaLatte: '#C4A169',
+        chocolate: '#583819',
+        softBlack: '#555555',
+        accent: colors.cyan.accent1,
+    },
+    events: {
+        eventName: "Xmas",
+        timeStamp: "12/25",
+        colors: {
+            hojichaLatte: '#C4A169',
+            chocolate: '#583819',
+            softBlack: '#555555',
+            accent: colors.cyan.accent1,
+        }
+    }
+}
