7266ac0097cb8b54d4f3bc862f6845b4b0d28f23
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-11-28 18:35:28 +0900

    Fix

--- src/plugins/themes.js
+++ src/plugins/themes.js
@@ -1,46 +1,45 @@
 import { colors } from 'vuetify/lib';
 
-export const themes = {
-    default: {
-        hojichaLatte: '#C4A169',
-        chocolate: '#583819',
-        softBlack: '#555555',
-        accent: colors.cyan.accent1,
-    },
-    events: [
-        {
-            eventName: "Xmas",
-            timeStamp: "2020-12-25",
-            colors: {
-                hojichaLatte: '#33743B',
-                chocolate: '#C92629',
-                softBlack: '#5B362C',
-                accent: '#9F863F',
-            }
-        },
-        {
-            eventName: "Halloween",
-            timeStamp: '2020-10-31',
-            colors: {
-                hojichaLatte: '#000000',
-                chocolate: '#FF6600',
-                softBlack: '#663399',
-                accent: '#FFBB00',
-            }
-        },
-    ]
+const defaultTheme = {
+    hojichaLatte: '#C4A169',
+    chocolate: '#583819',
+    softBlack: '#555555',
+    accent: colors.cyan.accent1,
 }
 
+const events = [
+    {
+        eventName: "Xmas",
+        timeStamp: "2020-12-25",
+        colors: {
+            hojichaLatte: '#33743B',
+            chocolate: '#C92629',
+            softBlack: '#5B362C',
+            accent: '#9F863F',
+        }
+    },
+    {
+        eventName: "Halloween",
+        timeStamp: '2020-10-31',
+        colors: {
+            hojichaLatte: '#000000',
+            chocolate: '#FF6600',
+            softBlack: '#663399',
+            accent: '#FFBB00',
+        }
+    },
+]
 
-export function getEventOrDefaultTheme() {
+
+export function getThemeByEvent() {
     const today = new Date();
-    const isEvent = themes.events.find((event) => {
+    const todaysEvent = events.find((event) => {
         const eventDay = new Date(event.timeStamp);
         return eventDay.getMonth() === today.getMonth()
             || eventDay.getDate() === today.getDate();
     });
-    if(isEvent) {
-        return isEvent.colors;
+    if(todaysEvent) {
+        return todaysEvent.colors;
     }
-    return themes.default;
+    return defaultTheme;
 }
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,14 +1,14 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
-import { getEventOrDefaultTheme } from './themes.js';
+import { getThemeByEvent } from './themes.js';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
     themes: {
-      light: getEventOrDefaultTheme(),
+      light: getThemeByEvent(),
     }
   },
   icons: {
