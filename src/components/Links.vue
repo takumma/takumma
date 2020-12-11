54a6d623ee1d8e59cf33b42bffa42dce3ddbbd5f
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-12-11 18:06:24 +0900

    Add: Links comp

---
+++ src/components/Links.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    v-for="link in links"
+    :key="link.icon"
+  >
+    <link-button :link="link"/>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Prop, Vue} from 'vue-property-decorator';
+import LinkButton from "@/components/LinkButton.vue";
+
+interface Link {
+  icon: string;
+  color: string;
+  url: string;
+}
+
+@Component({
+  components: {LinkButton}
+})
+export default class Links extends Vue {
+  @Prop()
+  readonly links: Link[] = [];
+}
+</script>
