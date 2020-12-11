12d42e09185a232b4a3f12ab113e37c0abcf7ea2
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-12-11 18:05:59 +0900

    Update: linkbtn

--- src/components/LinkButton.vue
+++ src/components/LinkButton.vue
@@ -1,18 +1,31 @@
 <template>
-  <v-btn>link</v-btn>
+  <v-btn
+      class="mx-2"
+      fab
+      dark
+      :color="link.color"
+      :href="link.url"
+  >
+    <v-icon
+      dark
+      x-large
+    >
+      {{ link.icon }}
+    </v-icon>
+  </v-btn>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import {Component, Prop, Vue} from 'vue-property-decorator';
 
 interface Link {
-	icon: string;
+  icon: string;
   color: string;
   url: string;
 }
 
 @Component
-export default class LinkButton extends Vue{
+export default class LinkButton extends Vue {
   @Prop()
   readonly link!: Link;
 }
